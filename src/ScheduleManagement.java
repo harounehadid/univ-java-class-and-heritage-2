d3d039e68228d5a2218739d2bb657f6b3260b96a
Haroune Hadid <harounehadidbusiness@gmail.com>
2022-10-29 11:29:42 +0100

    Add schedule cell class to ScheduleManagement class. Work on setting up the schedule

--- src/ScheduleManagement.java
+++ src/ScheduleManagement.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+
 public final class ScheduleManagement {
-    private class Schedule {
-        
+    private class ScheduleCell {
+        int startingHour;
+        int endingHour;
+        boolean occupied;
+        String acceptedEmissionTypes[];
+
+        public ScheduleCell(int startingHour, int endingHour, String acceptedEmissionTypes[]) {
+            this.startingHour = startingHour;
+            this.endingHour = endingHour;
+            this.occupied = false;
+            this.acceptedEmissionTypes = acceptedEmissionTypes;
+        }
+    }
+
+    private ArrayList<ScheduleCell> schedule;
+
+    public void launch() {
+        this.schedule = new ArrayList<ScheduleCell>();
+
+        initialScheduleSet();
+    }
+
+    private void initialScheduleSet() {
+        String emissionTypes[] = {"entertainment", "fiction", "reportage"};
     }
 }
